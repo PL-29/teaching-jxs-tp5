b3e4d37c2658b01485940e87c44e4e745fad838a
PL-29 <pl.ollivier29@gmail.com>
2017-02-08 15:38:38 +0100

    Partie 1 - Recherche dans une liste

--- js/pokedex.js
+++ js/pokedex.js
@@ -6,3 +6,16 @@ pokeApp.constant('POKEAPI', 'http://pokeapi.co');
 pokeApp.config(['$resourceProvider', function($resourceProvider) {
     $resourceProvider.defaults.stripTrailingSlashes = false;
 }]);
+
+pokeApp.controller('controller1', ['$scope', '$log', function($scope, $log) {
+  $scope.ecrireLogs = function(){
+	  $log.debug('Godis & PL');
+  }
+  
+  $scope.pokemons = [
+          {name:'pikachu', id:'1'},
+          {name:'carapuce', id:'2'},
+          {name:'salamèche', id:'3'},
+          {name:'bulebizar', id:'4'}]
+  
+}]);
